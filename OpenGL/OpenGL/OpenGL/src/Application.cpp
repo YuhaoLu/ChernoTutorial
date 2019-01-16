8839972eb46263f0f87b87de7ddd5697df6fd014
YuhaoLu <yuhao.lu@hotmail.com>
2019-01-15 20:31:26 -0800

    Add GL Math and MVP Tranformation

--- OpenGL/OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/OpenGL/src/Application.cpp
@@ -17,6 +17,8 @@
 
 #include "Texture.h"
 
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 
 int main(void)
 {
@@ -31,7 +33,7 @@ int main(void)
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -51,10 +53,10 @@ int main(void)
 
 	{ 
 		float positions[] = {
-			-0.5f, -0.5f, 0.0f, 0.0f, // 0
-			 0.5f, -0.5f, 1.0f, 0.0f, // 1
-			 0.5f,  0.5f, 1.0f, 1.0f, // 2
-			-0.5f,  0.5f, 0.0f, 1.0f  // 3
+			100.0f, 100.0f, 0.0f, 0.0f, // 0
+			200.0f, 100.0f, 1.0f, 0.0f, // 1
+			200.0f, 200.0f, 1.0f, 1.0f, // 2
+			100.0f, 200.0f, 0.0f, 1.0f  // 3
 		};
 
 		unsigned int indices[] = {
@@ -79,15 +81,23 @@ int main(void)
 		// 2. Index Buffers
 		IndexBuffer ib(indices, 6);
 
+		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+
+		glm::mat4 mvp = proj * view * model;
+
 		// 3. Shader
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
-		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f); // Uniforms
+		// Uniforms
+		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f); 
+		shader.SetUniformMat4f("u_MVP", mvp); // Model View Projection Matrix
 
+		// Texture
 		Texture texture("res/textures/ChernoLogo.png");
 		texture.Bind();
 		shader.SetUniform1i("u_Texture", 0);
-
 
 		va.Unbind();
 		vb.Unbind();
