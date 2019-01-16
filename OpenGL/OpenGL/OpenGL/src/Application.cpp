a8466a29fd12bd1c6e7bc21cca2e5292568b9d2a
YuhaoLu <yuhao.lu@hotmail.com>
2019-01-16 00:47:53 -0800

    Rendering multiple Objects

--- OpenGL/OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/OpenGL/src/Application.cpp
@@ -61,10 +61,10 @@ int main(void)
 
 	{ 
 		float positions[] = {
-			100.0f, 100.0f, 0.0f, 0.0f, // 0
-			200.0f, 100.0f, 1.0f, 0.0f, // 1
-			200.0f, 200.0f, 1.0f, 1.0f, // 2
-			100.0f, 200.0f, 0.0f, 1.0f  // 3
+		   -50.0f, -50.0f, 0.0f, 0.0f, // 0
+		    50.0f, -50.0f, 1.0f, 0.0f, // 1
+		    50.0f,  50.0f, 1.0f, 1.0f, // 2
+		   -50.0f,  50.0f, 0.0f, 1.0f  // 3
 		};
 
 		unsigned int indices[] = {
@@ -91,7 +91,7 @@ int main(void)
 
 		// Model View Projection Matrices
 		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 
 		// 3. Shader
 		Shader shader("res/shaders/Basic.shader");
@@ -123,7 +123,8 @@ int main(void)
 
 		float r = 0.0f;
 		float increment = 0.05f;
-		glm::vec3 translation(200, 200, 0);
+		glm::vec3 translationA(200, 200, 0);
+		glm::vec3 translationB(400, 200, 0);
 
 		/* Loop until the user closes the window */
 		while (!glfwWindowShouldClose(window))
@@ -135,16 +136,29 @@ int main(void)
 			ImGui_ImplOpenGL3_NewFrame();
 			ImGui_ImplGlfw_NewFrame();
 			ImGui::NewFrame();
-            
 
-			glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-			glm::mat4 mvp = proj * view * model;
 
-			shader.Bind();
-			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-			shader.SetUniformMat4f("u_MVP", mvp); // Model View Projection Matrix
+			{
+				// shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+				glm::mat4 mvp = proj * view * model;
+				shader.Bind();
+				shader.SetUniformMat4f("u_MVP", mvp); // Model View Projection Matrix
+				
+				renderer.Draw(va, ib, shader);
+			}
 
-			renderer.Draw(va, ib, shader);
+			// Draw the 2nd Object
+			{
+				// shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+				glm::mat4 mvp = proj * view * model;
+				shader.Bind();
+				shader.SetUniformMat4f("u_MVP", mvp); // Model View Projection Matrix
+
+				renderer.Draw(va, ib, shader);
+			}
+
 
 			if (r > 1.0f) {
 				increment = -0.05f;
@@ -159,9 +173,10 @@ int main(void)
 			{
 				static float f = 0.0f;
 
-				ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+				ImGui::Begin("Hello, world!");  // Create a window called "Hello, world!" and append into it.
 
-				ImGui::SliderFloat3("translation", &translation.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+				ImGui::SliderFloat3("translation A", &translationA.x, 0.0f, 960.0f);  // Edit 1 float using a slider from 0.0f to 1.0f
+				ImGui::SliderFloat3("translation B", &translationB.x, 0.0f, 960.0f);  // Edit 1 float using a slider from 0.0f to 1.0f
 
 				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 				ImGui::End();
